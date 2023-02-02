337fca65561cfb7bbf0c353a785a4c0598f3c206
Luigi <luigi.eduardo14@gmail.com>
2023-02-02 16:04:23 -0300

    Initial commit

---
+++ src/test/java/com/pimentas/usersmanagerservice/UsersManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimentas.usersmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
